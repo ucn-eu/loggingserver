4475a217371853bb530db611576e39a63b9771ae
Tom Lodge <tlodge@gmail.com>
2014-09-16 16:17:57 +0000

    cp commit

--- loggingserver.py
+++ loggingserver.py
@@ -3,7 +3,6 @@ import json
 import os
 import datetime
 from datetime import date
-<<<<<<< HEAD
 import requests
 import config
 import logging
@@ -21,6 +20,12 @@ logger.setLevel(logging.DEBUG)
 #clicks on this url and is then prompted to enter pin in moves app.
 @app.route("/", methods=['GET'])
 def root():
+	
+	print request
+	print request
+	print request.environ
+	print request.headers
+	
 	if len(request.access_route) > 1:
 		host = request.access_route[-1]
 	else:
@@ -79,17 +84,17 @@ def log():
 	
 	data = request.get_json(force=False)
 	datestr = datetime.datetime.now().strftime("%d-%m-%y_%H:%M:%S") 
-	remoteip = ""
+	host = ""
 	if len(request.access_route) > 1:
-		remoteip = request.access_route[-1]
+		host = request.access_route[-1]
 	else:
-		remoteip = request.access_route[0]
+		host = request.access_route[0]
 
 	logger.debug("received data for host %s" % host)
 	
 	try:
 		strdata = "%s" % json.dumps(data)
-		directory = "/var/log/netdata/%s" % (remoteip)
+		directory = "/var/log/netdata/%s" % (host)
 
 		if not os.path.exists(directory):
 			logger.debug("creating new dir %s" % directory)
@@ -111,34 +116,3 @@ if __name__ == "__main__":
 	authdb = AuthDB(name="auth.db")
 	#authdb.createTables()
 	app.run(host='0.0.0.0', port=8000, debug=True)
-=======
-
-app = Flask(__name__)
-
-@app.route("/log", methods=['POST'])
-def log():
-
-        data = request.get_json(force=False)
-        datestr = datetime.datetime.now().strftime("%d-%m-%y_%H:%M:%S")
-        print datestr
-        try:
-                strdata = "%s" % json.dumps(data)
-                directory = "/var/log/netdata/%s" % (request.remote_addr)
-
-                if not os.path.exists(directory):
-                        os.makedirs(directory)
-
-                with open("%s/%s.txt" % (directory, datestr), "w") as logfile:
-                        logfile.write(strdata)
-                        return jsonify(success=True)
-
-        except Exception as e:
-                print str(e)
-                return jsonify(success=False, error=str(e))
-
-        return jsonify(success=False)
-
-
-if __name__ == "__main__":
-        app.run(host='0.0.0.0', port=5000, debug=True)
->>>>>>> b6f6b7ff715ac9ebb4ad5531b09139935c698348
